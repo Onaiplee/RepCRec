bbc96a1bc224f07e393399b1e25445b4b80a6260
Shaobi Li <shaobili@gmail.com>
2010-11-12 19:10:50 -0500

    format input completed

---
+++ UIControl.rb
@@ -0,0 +1,64 @@
+require 'pp'
+
+class TM
+  def initialize
+  end
+  
+  def read(opts)
+    pp opts
+    return
+  end
+end
+    
+class UIController
+  def initialize(input = STDIN, output = STDOUT)
+    @inputFile = input
+    @outputFile = output
+    @pattern = /([\w]+)([\s]*)\(([\w\s,]*)\)/
+  end
+
+  def run
+    tm = TM.new
+    if @inputFile == STDIN
+      f = STDIN
+    else
+      f = File.open(@inputFile, "r")
+    end
+    f.each do |line|
+      next if line =~ /^\\\\/
+      opts = format_input(line)
+      out_put = tm.read(opts)
+      unless out_put.nil?
+        File.open(ouputFile, "a") do |log|
+          log.puts out_put
+        end
+      end
+    end
+    f.close
+  end
+
+ private
+  def format_input(line)
+    opts = line.split(';')
+    result = []
+    for op in opts
+      @pattern =~ op
+      data = Regexp.last_match
+      raise "bad arguments" if data.nil?
+      op_code, place_holder, params = data[1..3]
+      if op_code =~ /dump/
+        op_code << (params.length+1).to_s
+      end
+      params = params.split(",")
+      result << [op_code, params].flatten
+    end
+    result
+  end
+end
+
+
+##### Run it ####
+if $0 == __FILE__
+  g = UIController.new
+  g.run
+end
--- tm.rb
+++ tm.rb
@@ -2,7 +2,106 @@
 ########  Transaction manager                #########
 
 
-class tm
-  def initialize()
+class SortedList
+  def initialize
+    @data = []
+  end
+
+  def <<(element)
+    (@data << element).sort! {|m,n| n.length <=> m.length}
+  end
+
+  def empty?
+    @data.empty?
+  end
+end
+
+class LockWaitList < SortedList
+  def initialize
+    super
+  end
+
+  def <<(transaction)
+    (@data << transaction).sort! {|m,n| n.birth_time <=> m.birth_time}
+  end
+  
+  def oldest
+    @data[-1]
+  end
+  
+  def youngest
+    @data[0]
+  end
+end
+
+class Transaction
+  
+end
+
+class Lock
+  def initialize(lock_id)
+    @lock_id = lock_id
+    @state = :idle
+    @owner = SortedList.new()
+    @waiters = {}
+    @wait_read_transactions = SortedList.new()
+    @wait_write_transactions = SortedList.new()
+  end
+
+  def acqire_read(transaction)
+    if owner.include? transaction
+      transaction.do_pending_action()
+      return
+    end
+    
+    if @state == :idle
+      @state = :read
+      @owner << transaction 
+    elsif @state == :read
+      @owner << transaction
+      unless @wait_write_transactions.empty?
+        unless @wait_write_transactions.oldest.older_than?(transaction)
+          losers = @wait_write_transactions
+          @wait_write_transactions = []
+          loser.each
+      
+      if @wait_write_transactions.not_empty?
+        if 
+    
+
+class Transaction
+  def initialize(id, birth_time, action_abort, is_readonly=False)
+    @id = id
+    @birth_time = birth_time
+    @state = 'normal'
+    @pending_action = nil
+    @result_message = ""
+    @locks = []
+    @waiting_lock = nil
+    @action_abort = action_abort
+    @variables_visited = []
+    @site_visited = []
+    @wait_var = nil
+  end
+  
+  def do_pending_action
+    if @pending_action
+    end
+  end
+
+  def abort
+    action_abort
+  end
+
+  def release_locks
+    if @state == :blocked and waiting_lock
+      lock = @waiting_lock
+      lock.give_up_wait
+    end
+    @locks.each { |l| l.release }
+  end
+  
+  def older_than?(lock)
+    @birth_time < lock.birth_time
   end
 end
